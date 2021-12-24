16588148b0029dada8afc546b71284e0e868f17e
OussamaBouazza <oussama-bouazza@outlook.com>
2021-12-24 17:36:28 +0100

    suppression fichiers inutiles
    Suppression des fichiers crée pour le tp auparavant qui sont inutile pour l'app

--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO address (id, creation, content, auteur) VALUES (1, CURRENT_TIMESTAMP(), '57 boulevard demorieux', 'Charles');
-INSERT INTO address (id, creation, content, auteur) VALUES (2, CURRENT_TIMESTAMP(), '51 allee du gamay, 34080 montpellier', 'Lewis');
